15dfa982070f1baba304df5bdd4feb02eab18be9
MrArtga <artga03@gmail.com>
2022-12-07 14:01:34 +0300

    Убрать лишний код js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ const formPhone = document.querySelector(".lab-form-phone");
 const nameus = document.getElementById("nameUser");
 const phone = document.getElementById("phoneUser");
 
-const stepSimbols = /[A-Z]/;
-const lowerSimbols = /[a-z]/;
-const nums = /[0-9]/;
-console.log(stepSimbols)
 button.addEventListener("click", () => {
     if(button) {
         formName.innerHTML=nameus.value;
-        if(nums.test(phone)) {
-            formPhone.innerHTML=phone.value;
-        } else {
-            console.log("pizda")
-        }
+        formPhone.innerHTML=phone.value;
     }
 })
