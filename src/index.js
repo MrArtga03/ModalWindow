4699b7811cdab8878de6ff5c11b72413448e741a
MrArtga <artga03@gmail.com>
2022-12-08 22:20:57 +0300

    Изменить кнопку

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const button = document.querySelector(".button");
+//Переменные DOM элементов
+const button = document.getElementById("button");
 const formName = document.querySelector(".lab-form-name");
 const formPhone = document.querySelector(".lab-form-phone");
 const nameus = document.getElementById("nameUser");
 const phone = document.getElementById("phoneUser");
 
 button.addEventListener("click", () => {
-    if(button) {
-        formName.innerHTML=nameus.value;
-        formPhone.innerHTML=phone.value;
-    }
+    button.classList.add("active");
+    button.style.boxShadow = "none";
+    formName.innerHTML=nameus.value;
+    formPhone.innerHTML=phone.value;
 })
